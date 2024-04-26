66a2db9651e0ddad2160a52c95d9b9541822a5ee
Ashmin <ashmin2802@gmail.com>
2024-04-26 15:39:17 +0530

    header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { ViewComponent } from './view/view.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'log',component:LoginComponent},
+  {path:'reg',component:RegistrationComponent},
+  {path:'view/:id',component:ViewComponent},
+  {path:'wish',component:WishlistComponent},
+  {path:'cart',component:CartComponent},
+  {path:'checkout',component:CheckoutComponent},
+  {path:'prof',component:ProfileComponent},
+  {path:'**',redirectTo:''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { CartComponent } from './cart/cart.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ViewComponent } from './view/view.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    RegistrationComponent,
+    CartComponent,
+    WishlistComponent,
+    CheckoutComponent,
+    ViewComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
