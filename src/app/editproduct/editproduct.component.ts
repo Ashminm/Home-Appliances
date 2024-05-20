3f6471e0a38d685e4f5cd4cd524d314c503bc344
Ashmin <ashmin2802@gmail.com>
2024-05-20 23:20:35 +0530

    edit page

--- src/app/editproduct/editproduct.component.ts
+++ src/app/editproduct/editproduct.component.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ApiCallService } from '../services/api-call.service';
-import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-editproduct',
@@ -8,19 +7,51 @@ import { OnInit } from '@angular/core';
   styleUrls: ['./editproduct.component.css']
 })
 export class EditproductComponent implements OnInit {
+  // product:any[]=[]
+  products: any[] = [];
+  validCards: any;
+  currentPage: number = 1;
+  productsPerPage: number = 12;
+  totalPage: number = 0;
 
-  product:any
-  constructor(private api:ApiCallService){}
+  constructor(private api: ApiCallService) {}
 
-ngOnInit() {
- this.getAllProduct() 
-}
+  ngOnInit(){
+    this.getAllProducts();
+  }
 
-getAllProduct(){
-  this.api.getAllProducts().subscribe((res:any)=>{
-    this.product=res
-    console.log(this.product);
-  })
-}
+  getAllProducts() {
+    this.api.getAllProducts().subscribe((res: any) => {
+      this.products = res;
+      // this.product=res
+      this.calculateTotalPages();
+      this.updateValidCards();
+    });
+  }
 
+  calculateTotalPages(){
+    this.totalPage = Math.ceil(this.products.length / this.productsPerPage);
+  }
+
+  updateValidCards(){
+    const startIndex = (this.currentPage - 1) * this.productsPerPage;
+    const endIndex = startIndex + this.productsPerPage;
+    this.validCards = this.products.slice(startIndex, endIndex);
+    console.log(this.validCards);
+    
+  }
+
+  onNext(){
+    if (this.currentPage < this.totalPage) {
+      this.currentPage++;
+      this.updateValidCards();
+    }
+  }
+
+  onPrev(){
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updateValidCards();
+    }
+  }
 }
